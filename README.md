44c3a489693a34c1ee3022bb5be63f2f7d4239cd
iachievedream <iachievedream@gmail.com>
2020-04-02 02:42:41 +0800

    resume, vue and css

---
+++ README.md
@@ -0,0 +1,11 @@
+# iachievedrea.github.io
+<a href="https://iachievedream.github.io/">iachievedrea.github.io</a>
+
+~~~
+git clone https://github.com/iachievedream/iachievedream.github.io
+cd C:\AppServ\www\demo\iachievedrea.github.io
+git add . 
+git commit -a -m "update content"
+git remote add origin https://github.com/iachievedream/iachievedream.github.io
+git push -u origin master
+~~~
---
+++ html_vue.js
@@ -0,0 +1,34 @@
+new Vue({
+    el: '#resume',
+    data: {
+        name: {
+            chinese: '傅獻慶',
+            english: 'FU,SIAN-CING',
+            id: 'iachievedream',
+            content:'喜歡學習以及分享，透過自學往程式這條路的人。喜歡兩門科學，物理以及經濟學，且用程式來完成。'
+        },
+        contect:{
+            title:'contect',
+            github:'https://github.com/iachievedream',
+            githubicon:'picture/github.svg',
+            fb:'https://www.facebook.com/profile.php?id=100002572888672',
+            fb_incon:'picture/facebook.svg',
+            web:'https://physicalconceptsstation.wordpress.com/',
+            web_icon:'picture/internet.svg'
+        },
+        experience:{
+            title:'experience',
+            experience1:'機械操作人員',
+            experience2:'國立大學研究助理',
+            experience3:'前端工程師與測試工程師',
+            experience4:'後端工程師'
+        },
+        skills:{
+            title:'skills',
+            skills1:'html',
+            skills2:'CSS',
+            skills3:'PHP',
+            skills4:'MySQL'
+        }
+    }
+})
